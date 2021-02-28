eefd7f329c07412c07b6ae5efc36b8ede0550948
kubabuba <kuba200145@interia.pl>
2021-02-28 16:16:07 +0100

    lab1_zadania

--- main.py
+++ main.py
@@ -1,6 +1,87 @@
-import sys as system
+#zad1
+# a = 1
+# b = 3.45
+# napis = "Ala ma psa"
+#
+# print(napis)
+# print(a)
+# print(b)
 
-print(system.version)
+#zad2
+# a = 2
+# b = 5
+# c = 10
+#
+# dodawanie=a+b
+# print(dodawanie)
+#
+# odejmowanie=c-b
+# print(odejmowanie)
+#
+# mnozenie=a*b
+# print(mnozenie)
+#
+# dzielenie= c/a
+# print(dzielenie)
 
-a=2
-print(a)
+# #zad3
+# a=3
+# b=4
+# c=5
+# d=6
+# e=7
+# f=8
+#
+# a+=2
+# b-=3
+# c*=3
+# d/=2
+# e**=2
+# f%=3
+#
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+
+#zad4
+
+# from math import *
+#
+# potega=pow(e,10)
+# print(potega)
+#
+# pierwiastek=(log(5+sin(8)**2))**1/6
+# print(pierwiastek)
+
+#Zad5
+# a='JAKUB'
+# b='MIECZKOWSKI'
+# print(a.capitalize(),b.capitalize())
+
+#Zad6
+# a='la la la la la'
+# print(a.count("la"))
+
+#Zad7
+# imie='Adam Małysz'
+# print(imie[1],imie[10])
+
+#Zad8
+# imie='Adam Małysz'
+# print(imie.split())
+
+#Zad9
+# a='Marek'
+# b= 25
+# c=255
+#
+# print("Nazywam się %s %s" %('Marek','Kowalski'))
+# print(float(b))
+# print(hex(c))
+
+
+
+
