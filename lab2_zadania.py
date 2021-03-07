4e590795ff5333cb66bf42d08946af5b8869931b
Kuba200145 <60467070+Kuba200145@users.noreply.github.com>
2021-03-07 19:03:45 +0100

    lab2_zadania

---
+++ lab2_zadania.py
@@ -0,0 +1,98 @@
+#Zad1
+
+# lista=['siatkówka', 'koszykówka','piłka nożna','tenis ziemny' ]
+# lista.reverse()
+# print(lista)
+
+
+#Zad2
+# słownik={'np':'na przykład','nwm':'nie wiem','zw':'zaraz wracam','jj':'już jestem'}
+# print(słownik['np'])
+# print(słownik['nwm'])
+# print(słownik['jj'])
+# print(słownik['zw'])
+
+#Zad3
+# słownik={'Wiedźmin 3: Dziki Gon':' CD Projekt Red','Cyberpunk 2077':' CD Projekt Red', 'League of Legends':'Riot Games', 'Counter-Strike: Global Offensive':'Valve Corporation'}
+# ilosc_elementow= len(słownik)
+# print(ilosc_elementow)
+
+#Zad4
+# napis = input("Wpisz dowolny zdanie: ")
+# print(napis)
+# print(napis.count('a'))
+
+#Zad5
+# a = input("Wpisz pierwsza liczba calkowita: ")
+# b = input("Wpisz druga liczba calkowita: ")
+# c = input("Wpisz trzecia liczba calkowita: ")
+#
+# a= int(a)
+# b= int(b)
+# c= int(c)
+#
+# wynik =(pow(a,b))+c
+# print(wynik)
+
+#Zad6
+# a = input("Wpisz pierwsza liczba : ")
+# b = input("Wpisz druga liczba : ")
+# c = input("Wpisz trzecia liczba : ")
+#
+# a= int(a)
+# b= int(b)
+# c= int(c)
+#
+# if (a > b) & (a > c):
+#     print('Liczba' + str(a) + 'jest najwieksza' )
+# elif (b > a) & (b > c):
+#     print('Liczba' + str(b) + 'jest najwieksza')
+# elif (c > a) & (c > b):
+#     print('Liczba' + str(c) + 'jest najwieksza')
+# elif (a==b) & (b==c) & (c==a):
+#     print('Wszystkie trzy liczby sa sobie rowne')
+# else:
+#     print('Dwie liczby sa najwieksze oraz rowne sobie')
+
+#Zad7
+# lista= [2,4,3.5,5,7.5]
+# for x in lista:
+#     kwadrat=x**2
+#     print(kwadrat)
+
+# Zad8
+# a = 0
+# while a!= 10:
+#     if (a % 2 == 0):
+#         print(a)
+#     a+= 1
+
+#Zad9
+# for a in range(2):
+#     for b in range(6, 7):
+#         print(b * 'E')
+#     for c in range(1, 2):
+#         print(c * 'E')
+# for d in range(6, 7):
+#     print(d * 'E')
+
+#Zad10
+# import math
+#
+# a = input("Wpisz liczbe nieujemna : ")
+# a= int(a)
+#
+# if (a>=0):
+#     print(math.sqrt(a))
+# else:
+#     print('Wpisana liczba jest ujemna')
+
+
+
+
+
+
+
+
+
+
